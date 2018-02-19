051fd80d27d42a88d20c139528c9d986f3b0dbc5
Kirilloid <17madjoker@gmail.com>
2018-02-19 21:50:59 +0700

    Ограничение доступа Админ модуля

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace app\modules\admin;
+use yii\filters\AccessControl;
+use Yii;
 
 /**
  * admin module definition class
@@ -22,4 +24,28 @@ class Module extends \yii\base\Module
 
         // custom initialization code goes here
     }
+
+    public function behaviors()
+    {
+        return [
+            'access'    =>  [
+                'class' =>  AccessControl::className(),
+                'denyCallback'  =>  function($rule, $action)
+                {
+                    throw new \yii\web\NotFoundHttpException();
+                },
+                'rules' =>  [
+                    [
+                        'allow' =>  true,
+                        'matchCallback' =>  function($rule, $action)
+                        {
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
+    }
+
+
 }
